903da8c681ae1923c650c38a3e30b6eb9a8c422e
Andrei Belov <soltegm@gmail.com>
2024-05-12 16:28:27 +0300

    SLD-T-64 Thread impl

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ mod model;
 mod rest_api;
 mod time;
 
+use std::time::Duration;
+
 use axum::Router;
-use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
-use model::Process;
 use tokio::net::TcpListener;
 
-
 pub use self::error::{Error, Result};
-
-use serde::{Deserialize, Serialize};
-
 
 //use surrealdb::engine::local::Mem; uncomment after moving to in memory DB
 //use once_cell::sync::Lazy;
@@ -31,10 +27,34 @@ async fn main() -> Result<()> {
     let routes_all = Router::new()
         .merge(rest_api::routes(database));
 
+    let (close_tx, close_rx) = tokio::sync::oneshot::channel();
+
     let listener = TcpListener::bind("0.0.0.0:8050").await.unwrap();
-    axum::serve(listener, routes_all.into_make_service())
-        .await
-        .unwrap();
+
+    // -- without threads --
+
+//    axum::serve(listener, routes_all.into_make_service())
+//        .await
+//        .unwrap();
+
+    let server_handle = tokio::spawn(async {
+        axum::serve(listener, routes_all)
+            .with_graceful_shutdown(async move {
+                _ = close_rx.await;
+            })
+            .await
+            .unwrap();
+    });
+
+    println!("waiting 5 seconds");
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    println!("telling server to shutdown");
+    _ = close_tx.send(());
+
+    println!("waiting for server to gracefully shutdown");
+    _ = server_handle.await;
+
 
     // let _pp: Vec<Process> = database
     //     .conn
